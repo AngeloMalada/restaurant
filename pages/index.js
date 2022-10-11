a6ab59192a44724ebd4a380f941250ac194853c5
Angelo Malada <angelo.malada@gmail.com>
2022-10-11 21:25:25 +0200

    added favicon

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       <Head>
         <title>Restaurant</title>
         <meta name='description' content='Restaurant' />
-        <link rel='shortcut icon' href='/images/favicon.ico' />
+        <link rel='icon' href='favicon.png' />
       </Head>
     </div>
   );
