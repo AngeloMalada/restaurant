9668dd3e3e25e408ea49a1dace23ee403e5b249f
Angelo Malada <64404406+AngeloMalada@users.noreply.github.com>
2022-10-19 23:33:26 +0200

    Update [id].jsx

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -16,7 +16,7 @@ export default Product;
 
 export const getServerSideProps = async ({ params }) => {
   const res = await axios.get(
-    `http://localhost:3000/api/products/${params.id}`
+    `https://vercel.com/angelomalada/restaurant/api/products/${params.id}`
   );
   return {
     props: {
