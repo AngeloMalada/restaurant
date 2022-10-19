85a351f5ebbabfac78a445000b7c433d494c370e
Angelo Malada <64404406+AngeloMalada@users.noreply.github.com>
2022-10-19 23:33:01 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home({ productList }) {
 }
 
 export const getServerSideProps = async () => {
-  const res = await axios.get("http://localhost:3000/api/products");
+  const res = await axios.get("https://vercel.com/angelomalada/restaurant/api/products");
   return {
     props: {
       productList: res.data,
